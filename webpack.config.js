5f2600b1e631ef04aafc2bf5b7e3b6ade49270e0
OskarPihlak <oskar.pihlak@gmail.com>
2018-08-23 14:52:34 +0300

    4096: production preparations

--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,80 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const webpack = require('webpack');
+var path = require('path')
+var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+// Phaser webpack config
+var phaserModule = path.join(__dirname, '/node_modules/phaser/')
+var phaser = path.join(phaserModule, 'src/phaser.js')
+
+var definePlugin = new webpack.DefinePlugin({
+    __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')),
+    WEBGL_RENDERER: true, // I did this to make webpack work, but I'm not really sure it should always be true
+    CANVAS_RENDERER: true // I did this to make webpack work, but I'm not really sure it should always be true
+})
 
 module.exports = {
     entry: {
-        app: './src/index.js',
-        'production-dependencies': ['phaser']
+        app: [
+            'babel-polyfill',
+            path.resolve(__dirname, 'src/index.js')
+        ],
+        vendor: ['phaser']
     },
-
+    devtool: 'cheap-source-map',
     output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js'
+      pathinfo: true,
+        path: path.resolve(__dirname, 'dev'),
+        publicPath: './dev/',
+        library: '[name]',
+        libraryTarget: 'umd',
+        filename: '[name].js'
     },
-
+    watch: true,
+    plugins: [
+        definePlugin,
+        //new webpack.optimize.CommonsChunkPlugin({ name: 'vendor'/* chunkName= */, filename: 'vendor.bundle.js'/* filename= */ }),
+        new HtmlWebpackPlugin({
+            filename: '../index.html',
+            template: path.resolve(__dirname, 'src/index.html'),
+            chunks: ['vendor', 'app'],
+            chunksSortMode: 'manual',
+            minify: {
+                removeAttributeQuotes: false,
+                collapseWhitespace: false,
+                html5: false,
+                minifyCSS: false,
+                minifyJS: false,
+                minifyURLs: false,
+                removeComments: false,
+                removeEmptyAttributes: false
+            },
+            hash: false
+        }),
+        new BrowserSyncPlugin({
+            host: process.env.IP || 'localhost',
+            port: process.env.PORT || 3000,
+            server: {
+                baseDir: ['./', './dev']
+            }
+        })
+    ],
     module: {
         rules: [
-            {
-                test: /\.js$/,
-                include: path.resolve(__dirname, 'src/'),
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env']
-                    }
-                }
-            }
+            { test: /\.js$/, use: ['babel-loader'], include: path.join(__dirname, 'src') },
+            { test: /phaser-split\.js$/, use: ['expose-loader?Phaser'] },
+            { test: [/\.vert$/, /\.frag$/], use: 'raw-loader' }
         ]
     },
-    optimization: {
-        splitChunks: {
-            chunks: 'async',
-            minSize: 30000,
-            maxSize: 0,
-            minChunks: 1,
-            maxAsyncRequests: 5,
-            maxInitialRequests: 3,
-            automaticNameDelimiter: '~',
-            name: true,
-            cacheGroups: {
-                vendors: {
-                    test: /[\\/]node_modules[\\/]/,
-                    priority: -10
-                },
-                default: {
-                    minChunks: 2,
-                    priority: -20,
-                    reuseExistingChunk: true
-                }
-            }
-        }
+   /* node: {
+        fs: 'empty',
+        net: 'empty',
+        tls: 'empty'
     },
-    plugins: [
-        new CopyWebpackPlugin([
-            {
-                from: path.resolve(__dirname, 'index.html'),
-                to: path.resolve(__dirname, 'build')
-            },
-            {
-                from: path.resolve(__dirname, 'assets', '**', '*'),
-                to: path.resolve(__dirname, 'build')
-            }
-        ]),
-        new webpack.DefinePlugin({
-            'typeof CANVAS_RENDERER': JSON.stringify(true),
-            'typeof WEBGL_RENDERER': JSON.stringify(true)
-        }),
-    ],
-    devServer: {
-        contentBase: path.resolve(__dirname, 'build'),
-    }
-};
+    resolve: {
+        alias: {
+            'phaser': phaser,
+        }
+    }*/
+}
